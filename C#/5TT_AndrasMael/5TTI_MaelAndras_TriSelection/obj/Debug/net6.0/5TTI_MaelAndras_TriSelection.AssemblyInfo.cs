262ad82524514fd66def634ffffcdaa7cacd310e
VintagixDev <vintagixgaming@gmail.com>
2024-01-22 22:12:05 +0100

    update

--- C#/5TT_AndrasMael/5TTI_MaelAndras_TriSelection/obj/Debug/net6.0/5TTI_MaelAndras_TriSelection.AssemblyInfo.cs
+++ C#/5TT_AndrasMael/5TTI_MaelAndras_TriSelection/obj/Debug/net6.0/5TTI_MaelAndras_TriSelection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("5TTI_MaelAndras_TriSelection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b233d2a4929e08002b35c12134e91a97ab1030da")]
 [assembly: System.Reflection.AssemblyProductAttribute("5TTI_MaelAndras_TriSelection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("5TTI_MaelAndras_TriSelection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
