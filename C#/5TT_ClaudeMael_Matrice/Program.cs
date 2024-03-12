67b386549e55120959eb23d8b0ba8c9636cd172e
Claude Maël <230622@CCM-Asty-Moulin.local>
2024-03-12 10:35:02 +0100

    Matrices

---
+++ C#/5TT_ClaudeMael_Matrice/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _5TT_ClaudeMael_Matrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Methodes methodes = new Methodes();
+
+            int[,] firstMatrice = methodes.GenererMatrice(5, 3);
+            int[,] secondMatrice = methodes.GenererMatrice(5, 3);
+            Console.WriteLine(methodes.ConcateneMatrice(firstMatrice) + "\n\n");
+            Console.WriteLine(methodes.ConcateneMatrice(secondMatrice) + "\n\n");
+            int[,] finalMatrice = new int[firstMatrice.GetLength(0), firstMatrice.GetLength(1)];
+            bool addition = methodes.AdditionMatrices(firstMatrice, secondMatrice, ref finalMatrice);
+            if(addition)
+            {
+                Console.WriteLine(methodes.ConcateneMatrice(finalMatrice));
+            }
+            else
+            {
+                Console.WriteLine("non");
+            }
+        }
+    }
+}
