e09d8a8617a0fbc7f03aa2742bff1a1c0ad362e7
Claude Maël <230622@CCM-Asty-Moulin.local>
2024-03-26 10:50:18 +0100

    Adresse IP

---
+++ C#/5TT_ClaudeMael_AdressesIP/Methodes.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5TT_ClaudeMael_AdressesIP
+{
+    public struct Methodes
+    {
+
+        /// <summary>
+        /// Ajouter un nom dans la liste des noms
+        /// </summary>
+        /// <param name="nom">Nom à ajouter à noms</param>
+        /// <param name="pointer">pointer est différent (en dessous) de la taille de noms</param>
+        /// <param name="noms">tableau contenant tous les noms</param>
+        /// <returns></returns>
+        public bool AjouteNom(string nom, int pointer, ref string[] noms)
+        {
+            if(pointer == noms.Length)
+            {
+                noms[pointer] = nom;
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Ajouter une adresse IP dans la matrice contenant toutes les adresses IP.
+        /// </summary>
+        /// <param name="ip">IP à ajouter à la matrice</param>
+        /// <param name="pointer">pointer est différent (en dessous) du nombre de lignes d'adressesIp</param>
+        /// <param name="adressesIp">matrice contenant toute les IPs</param>
+        /// <returns></returns>
+        public bool AjouteAdresseIp(byte[] ip, ref int pointer, ref byte[,] adressesIp)
+        {
+            if(!(pointer == adressesIp.GetLength(0)))
+            {
+                pointer++;
+                for(int i = 0; i < adressesIp.GetLength(1); i++)
+                {
+                    adressesIp[pointer, i] = ip[i];
+                }
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Lire l'entrée utilisateur d'une IP entière
+        /// </summary>
+        /// <param name="ip">variable contenant l'IP finale</param>
+        public void LireAdresseIp(out byte[] ip)
+        {
+            ip = new byte[4];
+            byte octet;
+            for(int i = 0; i < 4; i++)
+            {
+                LireOctet("Veuillez entrer l'octet n°" + i + " de votre adresse IP", out octet);
+                ip[i] = octet;
+            }
+        }
+
+        /// <summary>
+        /// Retourne une adresse IP concaténée
+        /// </summary>
+        /// <param name="ip">adresse IP à concaténer</param>
+        /// <returns></returns>
+        public string ConcateneAdresse(byte[] ip)
+        {
+            string message = "";
+            for(int i = 0; i < ip.Length; i++)
+            {
+                message += ip[i];
+                if(i < ip.Length - 1)
+                {
+                    message += ".";
+                }
+            }
+            return message;
+        }
+
+        /// <summary>
+        /// Lire l'entrée utilisateur (octet)
+        /// </summary>
+        /// <param name="question">Question posée à l'utilisateur</param>
+        /// <param name="octet">Variable contenant l'octet entré</param>
+        public void LireOctet(string question, out byte octet)
+        {
+            Console.WriteLine(question);
+            string nUser = Console.ReadLine();
+            while(!byte.TryParse(nUser, out octet))
+            {
+                Console.WriteLine(question);
+                nUser = Console.ReadLine();
+            }
+        }
+
+
+        public string ConcateneTout(string[] noms, int pointer, byte[,] adressesIp)
+        {
+            string message = "";
+            byte[] ip = new byte[3];
+
+
+            for(int i = 0; i < pointer; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    ip[j] = adressesIp[i, j];
+                }
+                message += noms[pointer] + ": " + ConcateneAdresse(ip) + "\n";
+            }
+            return message;
+        }
+
+    }
+}
---
+++ C#/5TT_ClaudeMael_AdressesIP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace _5TT_ClaudeMael_AdressesIP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int maxLength = 20;
+
+            byte[,] adressesIp = new byte[maxLength, 3];
+            int pointer = 0;
+           
+
+            Methodes methodes = new Methodes();
+
+            methodes.LireAdresseIp(out byte[] ip);
+            Console.WriteLine(methodes.ConcateneAdresse(ip));
+
+        }
+    }
+}
